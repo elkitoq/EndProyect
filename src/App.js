f7112c9c77f995ad2bf5716d3153ba1841e311d7
elkitoq <elkitoq@gmail.com>
2021-05-09 16:52:36 -0300

    update viewlogin

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { ViewLogin } from './Views/ViewLogin.js'
 
 function App() {
--- src/Views/ViewLogin.js
+++ src/Views/ViewLogin.js
@@ -1,19 +1,24 @@
-import { Form, FormGroup } from 'reactstrap'
+import { Form, FormGroup, Container } from 'reactstrap'
 import { Buttons } from '../Components/Buttons.js'
 import { Inputs } from '../Components/Inputs.js'
 import { Labels } from '../Components/Labels.js'
 
 export const ViewLogin = () => {
     return (
-        <Form>
-            <FormGroup>
-                <Labels name="Usuario"></Labels>
-                <Inputs classname="inputUser" />
-            </FormGroup>
-            <FormGroup>
-                <Labels name="Password"></Labels>
-                <Inputs type="password" classname="inputPassword"></Inputs>
-            </FormGroup>
-        </Form>
+        <Container>
+            <Form>
+                <FormGroup>
+                    <Labels name="Usuario"></Labels>
+                    <Inputs classname="inputUser" />
+                </FormGroup>
+                <FormGroup>
+                    <Labels name="Password"></Labels>
+                    <Inputs type="password" classname="inputPassword"></Inputs>
+                </FormGroup>
+                <FormGroup>
+                    <Buttons name="Login" size="lg" color="primary"></Buttons>
+                </FormGroup>
+            </Form>
+        </Container>
     )
 }
