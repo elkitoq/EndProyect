cd03114c09e2c6c53e918197ad0fd3c1dbf22d6b
elkitoq <elkitoq@gmail.com>
2021-05-09 17:37:12 -0300

    new uptdate login view

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -1,7 +1,7 @@
 import { Button } from 'reactstrap'
 
-export const Buttons = ({ name, color, size }) => {
+export const Buttons = ({ name, color, size, blocks }) => {
     return (
-        <Button color={color} size={size}>{name}</Button>
+        <Button color={color} size={size} block>{name}</Button>
     )
 }
--- src/Views/ViewLogin.js
+++ src/Views/ViewLogin.js
@@ -1,24 +1,28 @@
-import { Form, FormGroup, Container } from 'reactstrap'
+import { Form, FormGroup, Container, Row, Col } from 'reactstrap'
 import { Buttons } from '../Components/Buttons.js'
 import { Inputs } from '../Components/Inputs.js'
 import { Labels } from '../Components/Labels.js'
 
 export const ViewLogin = () => {
     return (
-        <Container>
-            <Form>
-                <FormGroup>
-                    <Labels name="Usuario"></Labels>
-                    <Inputs classname="inputUser" />
-                </FormGroup>
-                <FormGroup>
-                    <Labels name="Password"></Labels>
-                    <Inputs type="password" classname="inputPassword"></Inputs>
-                </FormGroup>
-                <FormGroup>
-                    <Buttons name="Login" size="lg" color="primary"></Buttons>
-                </FormGroup>
-            </Form>
+        <Container className="themed-container">
+            <Row>
+                <Col sm="12" md={{ size: 4, offset: 4 }}>
+                    <Form>
+                        <FormGroup>
+                            <Labels name="Usuario"></Labels>
+                            <Inputs classname="inputUser" />
+                        </FormGroup>
+                        <FormGroup>
+                            <Labels name="Password"></Labels>
+                            <Inputs type="password" classname="inputPassword"></Inputs>
+                        </FormGroup>
+                        <FormGroup>
+                            <Buttons name="Login" size="lg" color="primary" blocks="true"></Buttons>
+                        </FormGroup>
+                    </Form>
+                </Col>
+            </Row>
         </Container>
     )
 }
