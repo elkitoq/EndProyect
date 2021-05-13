e544ffa03dd5f6140a03c16b30931600f905e5d5
Rey Cristian <rey.cristian.eze@gmail.com>
2021-05-13 11:57:50 -0300

    concurrently para que ande server y cliente

--- src/Server/routes/routes.js
+++ src/Server/routes/routes.js
@@ -1,6 +1,23 @@
 
 const router = require('express').Router();
 
-router.use(require("./servidorBase.js"));
+const fs = require('fs');
+
+fs.readdir("./src/server/routes", function (err, archivos) {
+if (err) {
+onError(err);
+return;
+}
+for(archivo of archivos){
+    if (archivo != 'routes.js'){
+        console.log('\x1b[33m%s\x1b[0m', `Cargando ruta del servidor: "${archivo}"`);
+        router.use(require("./servidorBase.js"));
+    }
+}
+
+console.log('\x1b[32m%s\x1b[0m', `Rutas cargadas`);
+
+});
+
 
 module.exports = router;
