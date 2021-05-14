18c0dc36af8ebdf9781e6ed3013c53225ce307f1
Rey Cristian <rey.cristian.eze@gmail.com>
2021-05-13 22:15:17 -0300

    routes en React, segun la idea del Kito. aun no le doy forma

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ViewLogin } from './Views/ViewLogin.js'
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+
+
 function App() {
   return (
-    <div className="App">
-      <ViewLogin />
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Login">Login</Link>
+          </li>
+          <li>
+            <Link to="/CVCreate">Crear CV</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/Login">
+            <ViewLogin />
+          </Route>
+          <Route path="/CVCreate">
+            Crear CV
+          </Route>
+          <Route path="/">
+            <h1>HOLA MUNDO</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+<div className="App">
+
+
+
+</div>
