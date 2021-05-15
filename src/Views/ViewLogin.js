928650f1e360e73e25309a138057cd42cbfb456e
elkitoq <elkitoq@gmail.com>
2021-05-15 19:20:52 -0300

    cambio de metodo de obtencion de datos

--- src/Views/ViewLogin.js
+++ src/Views/ViewLogin.js
@@ -1,30 +1,43 @@
+import { useState } from 'react'
 import { Form, FormGroup, Container, Row, Col, Button, Input, Label } from 'reactstrap'
 import md5 from 'md5'
 
 
 export const ViewLogin = () => {
 
-    const getUserData = () => {
-        let user = document.getElementById('userInput').value
-        let userPass = md5(document.getElementById('userPassword').value)
-        console.log(user, userPass)
+    const [datos, setdatos] = useState({
+        user: '',
+        password: ''
+    })
+
+    const handleInputChange = (event) => {
+        setdatos({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const submit = (event) => {
+        event.preventDefault()
+        //aqui iria la llamada al server
+        console.log(datos)
+
     }
 
     return (
         <Container className="themed-container">
             <Row>
                 <Col sm="12" md={{ size: 4, offset: 4 }}>
-                    <Form className="form-container">
+                    <Form className="form-container" onSubmit={submit}>
                         <FormGroup>
                             <Label for="userInput">Usuario</Label>
-                            <Input type="text" id="userInput" className="inputUser" name="user" />
+                            <Input type="text" id="userInput" name="user" onChange={handleInputChange} />
                         </FormGroup>
                         <FormGroup>
                             <Label for="userPassword">Password</Label>
-                            <Input type="password" id="userPassword" className="form-control" name="password" />
+                            <Input type="password" id="userPassword" name="password" onChange={handleInputChange} />
                         </FormGroup>
                         <FormGroup>
-                            <Button size="lg" color="primary" onClick={getUserData}>Login</Button>
+                            <Button size="lg" color="primary" type="submit">Login</Button>
                         </FormGroup>
                     </Form>
                 </Col>
