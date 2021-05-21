10784b0a28f2bc5d0497cb8ff0f0bad265b33d34
elkitoq <elkitoq@gmail.com>
2021-05-20 21:06:45 -0300

    agregado botones de login con facebook y google

--- src/Views/ViewLogin.js
+++ src/Views/ViewLogin.js
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import { Form, FormGroup, Container, Row, Col, Button, Input, Label } from 'reactstrap'
+import FacebookLogin from 'react-facebook-login';
+import { GoogleLogin } from 'react-google-login';
+import '../Assets/Css/login.css'
 
 
 
@@ -24,11 +27,27 @@ export const ViewLogin = () => {
 
     }
 
+    const responseFacebook = (response) => {
+        console.log(response.name, response.email, response.picture.data.url);
+
+    }
+
+    // const componentClicked = () => {
+    //     //se agregaria informacion a la cookie 
+    // }
+
+    const responseGoogle = (response) => {
+        console.log(response)
+        console.log(response.profileObj)
+        //se agregaria informacion a la cookie usando el response.profileObj que da mas datos sobre el usuario
+    }
+
     return (
         <Container className="themed-container">
-            <Row>
-                <Col sm="12" md={{ size: 4, offset: 4 }}>
+            <Row className="row-login">
+                <Col className="column" sm="12" md={{ size: 4, offset: 4 }}>
                     <Form className="form-container" onSubmit={submit}>
+
                         <FormGroup>
                             <Label for="userInput">Usuario</Label>
                             <Input type="text" id="userInput" name="user" onChange={handleInputChange} />
@@ -37,10 +56,33 @@ export const ViewLogin = () => {
                             <Label for="userPassword">Password</Label>
                             <Input type="password" id="userPassword" name="password" autoComplete="off" onChange={handleInputChange} className="form-control" />
                         </FormGroup>
+                        <div>
+                            <FacebookLogin
+                                appId="323151842756210"
+                                autoLoad={false}
+                                fields="name,email,picture"
+                                callback={responseFacebook}
+                                cssClass="my-facebook-button-class"
+                                icon="fa-facebook"
+                                size="medium"
+                                textButton="acebook"
+
+                            />
+                            <GoogleLogin
+                                className="butonLoginGoogle"
+                                clientId="1095958975836-nnf32pq1spueun0hcgmjajiaf3q5s39s.apps.googleusercontent.com"
+                                onSuccess={responseGoogle}
+                                onFailure={responseGoogle}
+                                cookiePolicy={'single_host_origin'}
+                                theme="ligh"
+                                buttonText="Login"
+                            />
+                        </div>
                         <FormGroup>
-                            <Button size="lg" color="primary" type="submit">Login</Button>
+                            <Button className='button-submit' size="lg" color="primary" type="submit">Login</Button>
                         </FormGroup>
                     </Form>
+
                 </Col>
             </Row>
         </Container>
